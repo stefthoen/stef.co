edd640fedc0c04493a4ad29626a5d3d1daa07643
Stef Thoen <stef@baardbaard.nl>
2022-05-20 15:40:41 +0200

    Initial setup Svelte

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+const config = {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+
+  theme: {
+    colors: {
+      'blue': '#1fb6ff',
+    },
+    extend: {}
+  },
+
+  plugins: []
+};
+
+module.exports = config;
